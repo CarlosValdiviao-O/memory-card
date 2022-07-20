eb6929ab085adbebd1b0a4ad843dcc732c195f4a
Carlos <carlosvaldivia390@gmail.com>
2022-07-20 15:42:32 -0400

    Added diplay random logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import { getDatabase } from "./database";
 
@@ -6,6 +6,9 @@ const Game = () => {
     let database = getDatabase();
     const [ clicked, setClicked] = useState([]);
     const [ notClicked, setNotClicked] = useState(database);
+    const [ all, setAll ] = useState(clicked.concat(notClicked));
+    const [ display, setDisplay ] = useState([]);
+    let aux = all;
 
     const onClick = (pick) => {
         if (pick.clicked === false) {
@@ -21,17 +24,36 @@ const Game = () => {
         else {
             setClicked([]);
             setNotClicked(database);
-        }
+        };
     }
+
+    useEffect(() => {
+        setDisplay([]);
+        setAll(clicked.concat(notClicked));
+        setTimeout(() => {
+            aux = clicked.concat(notClicked);
+            setDisplay(fillWithRandom());
+        }, 300)
+    }, [clicked])
+
+    const fillWithRandom = () => {
+        let random = [];
+        let isOneNotClicked = false;
+        for (let i = 0; i < 12; i++) {
+            let ind = Math.floor(Math.random() * aux.length);
+            random.push(aux[ind]);
+            if (aux[ind].clicked === false) isOneNotClicked = true;
+            aux.splice(ind, 1); 
+        }
+        if (isOneNotClicked === false) random = fillWithRandom();
+        return random;
+    }
+
     return (
         <div>
-            <h3>Clicked</h3>
-            {clicked.map((item) => {
-                return <Card key = {item.id} item = {item} onClick = {onClick}/>
-            })}
-            <h3>Not Clicked</h3>
-            {notClicked.map((item) => {
-                return <Card key = {item.id} item = {item} onClick = {onClick}/>
+            <h3>{clicked.length}</h3>
+            {display.map((item) => {
+                return <Card key = {item.id+clicked.length} item = {item} onClick = {onClick}/>
             })}
         </div>
     )
